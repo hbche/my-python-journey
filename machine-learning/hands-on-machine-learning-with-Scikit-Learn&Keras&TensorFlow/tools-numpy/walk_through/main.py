98300fbb2fd81ff1fbaf5589041f7cc8783cd72a
hanbin.che <hanbin.che@qingteng.cn>
2026-03-09 15:49:04 +0800

    机器学习实战 - numpy 前置知识学习

--- machine-learning/hands-on-machine-learning-with-Scikit-Learn&Keras&TensorFlow/tools-numpy/walk_through/main.py
+++ machine-learning/hands-on-machine-learning-with-Scikit-Learn&Keras&TensorFlow/tools-numpy/walk_through/main.py
@@ -1,48 +1,14 @@
 import numpy as np
 
-q1 = np.full((3, 4), 1.0)
-print(q1)
-# [[1. 1. 1. 1.]
-#  [1. 1. 1. 1.]
-#  [1. 1. 1. 1.]]
+a = np.random.rand(2, 3)
+b = np.arange(24, dtype=np.uint8).reshape(2, 3, 4)
 
-q2 = np.full((4, 4), 2.0)
-print(q2)
-# [[2. 2. 2. 2.]
-#  [2. 2. 2. 2.]
-#  [2. 2. 2. 2.]
-#  [2. 2. 2. 2.]]
+np.savez("my_array.npz", my_a=a, my_b=b)
 
-q3 = np.full((3, 4), 3.0)
-print(q3)
-# [[3. 3. 3. 3.]
-#  [3. 3. 3. 3.]
-#  [3. 3. 3. 3.]]
+my_arrays = np.load("my_array.npz")
+print(my_arrays.keys())
+# KeysView(NpzFile 'my_array.npz' with keys: my_a, my_b)
 
-q4 = np.vstack((q1, q2, q3))
-print(q4.shape)  # (10, 4)
-print(q4)
-# [[1. 1. 1. 1.]
-#  [1. 1. 1. 1.]
-#  [1. 1. 1. 1.]
-#  [2. 2. 2. 2.]
-#  [2. 2. 2. 2.]
-#  [2. 2. 2. 2.]
-#  [2. 2. 2. 2.]
-#  [3. 3. 3. 3.]
-#  [3. 3. 3. 3.]
-#  [3. 3. 3. 3.]]
-
-q5 = np.hstack((q1, q3))
-print(q5.shape)  # (3, 8)
-print(q5)
-
-# [[1. 1. 1. 1. 3. 3. 3. 3.]
-#  [1. 1. 1. 1. 3. 3. 3. 3.]
-#  [1. 1. 1. 1. 3. 3. 3. 3.]]
-
-q5 = np.concatenate((q1, q2, q3), axis=0)  # 等价于 vstack
-print(q5.shape) # (10, 4)
-
-# q5 = np.concatenate((q1, q2, q3), axis=1)
-# print(q5.shape)
+print(my_arrays["my_a"])
+# [[0.38080268 0.79826849 0.87967549]
+#  [0.35814068 0.13948382 0.69677489]]
